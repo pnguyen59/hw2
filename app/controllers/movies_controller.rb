2bffc86e0c9a85f1ffecd543c15a15a30abd0845
pnguyen59 <thienphucnguyenchau@gmail.com>
2017-02-18 02:01:20 +0000

    Final commit

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,5 @@
 class MoviesController < ApplicationController
-
+  
   def movie_params
     params.require(:movie).permit(:title, :rating, :description, :release_date)
   end
@@ -9,11 +9,56 @@ class MoviesController < ApplicationController
     @movie = Movie.find(id) # look up movie by unique ID
     # will render app/views/movies/show.<extension> by default
   end
-
+  
   def index
-    @movies = Movie.all
-  end
+    redi = false
+    if params[:ratings]!=nil
+       @ratingKey = params[:ratings]
+       if @ratingKey.is_a?(Hash)
+        @ratingKey = @ratingKey.keys
+       end
+      session[:ratingKey] = @ratingKey
+       @movies = Movie.where({rating: @ratingKey})
 
+    elsif session[:ratingKey]!= nil
+      @ratingKey = session[:ratingKey]
+      @movies = Movie.where({rating: @ratingKey})
+      redi =true
+    else
+        @movies = Movie.all
+      @ratingKey=Movie.getRatings
+    end
+    
+    @all_ratings= Movie.getRatings
+    @movies = @movies.order(params[:sort_by])
+    @sort_t =params[:sort_by]
+  
+    if @sort_t != nil
+      if @sort_t == 'title'
+      @title_header = 'hilite'
+      session[:sort_t] = 'title'
+      else
+        @release_header ='hilite'
+        session[:sort_t] = 'release_date'
+      end
+    elsif session[:sort_t]!= nil
+      if session[:sort_t] == 'title'
+        @sort_t =session[:sort_t]
+        @title_header = 'hilite'
+        redi =true
+      else
+         @sort_t =session[:sort_t]
+      @release_header ='hilite'
+      redi = true
+      end 
+    end
+    
+    if redi == true
+      session.clear
+      flash.keep
+      redirect_to movies_path(sort_by: @sort_t, ratings: @ratingKey )
+    end
+  end
   def new
     # default: render 'new' template
   end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,5 @@
 class Movie < ActiveRecord::Base
+    def self.getRatings
+        return Movie.pluck(:rating).uniq
+    end
 end
